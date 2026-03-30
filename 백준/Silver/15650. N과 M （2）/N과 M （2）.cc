e46cb1e79b51d65baea26b0efde0952744f41324
Minseo Kim <kimminss0@outlook.kr>
2026-03-30 16:06:35 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int data;
+    void *next;
+} N_;
+
+void func(N_*, int, int, int, int);
+int main(void)
+{
+    int N, M;
+    scanf("%d %d", &N, &M);
+    func(NULL, 1, N, M, 0);
+    return 0;
+}
+
+void func(N_* foo, int min, int N, int M, int len)
+{
+    if(len == M)
+    {
+        int* arr = (int*)malloc(sizeof(int) * M);
+        while(len--)
+        {
+            arr[len] = foo->data;
+            foo = (N_*)(foo->next);
+        }
+        for(int i = 0; i < M; ++i)
+            printf("%d ", arr[i]);
+        printf("\n");
+        return;
+    }
+    while(min <= N)
+    {
+        int new_min = min + 1;
+        N_ bar;
+        bar.data = min;
+        bar.next = (void*)foo;
+        func(&bar, new_min, N, M, len + 1);
+        ++min;
+    }
+}
