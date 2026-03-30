dc5ebe4d1da46a68f1647b19959f60f0d201d4ab
Minseo Kim <kimminss0@outlook.kr>
2026-03-30 16:01:14 +0900

    [Gold V] Title: Moo 게임, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Gold/5904. Moo 게임/Moo 게임.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int len(int k);
+char func(int N);
+
+int main(void) {
+    int N;
+    scanf("%d", &N);
+    printf("%c", func(N));
+}
+
+int len(int k) {
+    if (k == 0) {
+        return 3;
+    }
+    return len(k-1) * 2 + (k+3);
+}
+
+char func(int N) {
+    int k, tmp;
+    for (k = 0; N >= len(k); ++k) {
+        ;
+    }
+    if (k == 0) {
+        return (N == 1) ? 'm' : 'o';
+    }
+    tmp = len(k-1) + (k+3);
+    if (N <= tmp) {
+        return (N==len(k-1)+1) ? 'm' : 'o';
+    }
+    return func(N-tmp);
+}
